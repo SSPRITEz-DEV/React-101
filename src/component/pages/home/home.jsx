4d368bd65429485567802241adeb257e559203a6
unknown <tanawatt2541@gmail.com>
2020-08-25 12:26:53 +0700

    try2

---
+++ src/component/pages/home/home.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+
+const Home = (
+    class Timer extends React.Component {
+        constructor(props) {
+          super(props);
+          this.state = { seconds: 0 };
+        }
+      
+        tick() {
+          this.setState(state => ({
+            seconds: state.seconds + 1
+          }));
+        }
+      
+        componentDidMount() {
+          this.interval = setInterval(() => this.tick(), 1000);
+        }
+      
+        componentWillUnmount() {
+          clearInterval(this.interval);
+        }
+      
+        render() {
+          return (
+            <div>
+              Seconds: {this.state.seconds}
+            </div>
+          );
+        }
+      }
+    
+);
+
+export default Home;
